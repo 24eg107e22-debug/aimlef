6f6410ebcef153f7fbac7962af47e6faeb07a2b4
vikramdev772 <vikramdev772@gmail.com>
2026-08-01 10:28:59 +0530

    Entity ✅

---
+++ backend/src/main/java/jar/model/Student.java
@@ -0,0 +1,20 @@
+package jar.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String name;
+    String email;
+    String ip;
+
+}
